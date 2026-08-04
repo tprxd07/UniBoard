cdbce88f02559d4c77f41012e519ca41d2a64893
tprxd07 <tomaspere07@gmail.com>
2026-08-04 02:22:33 +0200

    Utils: Buenas noches from 21:00

--- js/utils.js
+++ js/utils.js
@@ -117,7 +117,7 @@ const Utils = {
     getGreeting() {
         const hour = new Date().getHours();
         if (hour >= 6 && hour < 13) return 'Buenos días';
-        if (hour >= 13 && hour < 20) return 'Buenas tardes';
+        if (hour >= 13 && hour < 21) return 'Buenas tardes';
         return 'Buenas noches';
     },
 
